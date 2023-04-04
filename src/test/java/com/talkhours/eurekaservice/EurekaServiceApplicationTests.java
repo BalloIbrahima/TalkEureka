a0e8ccfee1f9479d1e11a5b6616bac9e052d8a56
BalloIbrahima <ibrahimaballo01@gmail.com>
2023-04-04 12:09:41 +0000

    init and config euraka

---
+++ src/test/java/com/talkhours/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talkhours.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
